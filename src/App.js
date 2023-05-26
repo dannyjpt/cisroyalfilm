6a6d8bbbb8d0ddd1afa18f45ca922b3bcef09ce0
dannyjpt <dpuerta27@gmail.com>
2023-05-25 22:35:56 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Navigation } from './components/navigation/nav'
 import Register from './components/register/register'
 import Login from './components/auth/auth'
 import Booking from './components/booking/booking'
+import Results from './components/results/results'
 import { FilmDetail } from './components/film_detal/film_detal'
 import {Routes, Route} from 'react-router-dom'
 
@@ -23,6 +24,7 @@ function App() {
       <Route path='/login' element={<><Navigation/><Login/></>}></Route>
       <Route path='/details/:id_film' element={<><Navigation/><FilmDetail/></>}></Route>
       <Route path='/booking/:id_category' element={<Booking/>}></Route>
+      <Route path='/results' element={<Results/>}></Route>
     </Routes>
   );
 }
